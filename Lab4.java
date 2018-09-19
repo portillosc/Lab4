a2e5b19f21b60ee0bdf74ae377fc356e1626d7c2
Sam <sam@MacBook-2.local>
2018-09-19 12:52:11 -0400

    pretty much finished Lab4 part 1

--- Lab4.java
+++ Lab4.java
@@ -9,32 +9,30 @@ public class Lab4{
       int[] a5 = {5,5,5,1,3};
       int[] a6 = null; 
       
-      int[] b1 = sort(a1);
-      int[] b2 = sort(a2);
-      int[] b3 = sort(a3);
-      int[] b4 = sort(a4);
-      int[] b5 = sort(a5);
-      int[] b6 = sort(a6);
-      
-   }
-   public static int[] sort(int[] a){
       try{
-      
-         int curr = 0;
-      
-         for(int i = 0; i < a.length; i++){
-            for(int j = i + 1; j < a.length; j++){
-               if(a[j] > a[i]){
-                  curr = a[i];
-                  a[i] = a[j];
-                  a[j] = curr;
-               }
-            } 
-         }
-         return a;
+         int[] b1 = sort(a1);
+         int[] b2 = sort(a2);
+         int[] b3 = sort(a3);
+         int[] b4 = sort(a4);
+         int[] b5 = sort(a5);
+         int[] b6 = sort(a6);
       }
       catch(NullPointerException e){
          System.out.println("Null array entered");
+      }
+      
+   }
+   public static int[] sort(int[] a){
+      int curr = 0;
+      
+      for(int i = 0; i < a.length; i++){
+         for(int j = i + 1; j < a.length; j++){
+            if(a[j] > a[i]){
+               curr = a[i];
+               a[i] = a[j];
+               a[j] = curr;
+            }
+         } 
       }
       return a;
    }
